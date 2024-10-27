20b32b364a75d0e096d5c2b1ed9a8b798ace1260
SantoshThapaa <st708611@gmail.com>
2024-10-27 22:38:38 +0545

    normal changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,41 @@
-
+// Import necessary modules
 import Home from "./components/Home";
 import Login from "./components/auth/Login";
 import Signup from "./components/auth/Signup";
+// import NotFound from "./components/NotFound"; // Import the NotFound component
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { I18nextProvider } from 'react-i18next'; // Import I18nextProvider
+import i18next from './i18n'; // Import your i18n configuration
 
-
+// Set up routes
 const appRouter = createBrowserRouter([
   {
     path: '/',
-    element:<Home/>
+    element: <Home />
   },
   {
     path: '/login',
-    element:<Login/>
+    element: <Login />
   },
   {
     path: '/signup',
-    element:<Signup/>
+    element: <Signup />
   },
-])
+  // {
+  //   path: '*',
+  //   element: <NotFound /> // Add a 404 route for undefined paths
+  // }
+]);
 
+// Define App component
 function App() {
-  
   return (
-    <div>
-      <RouterProvider router={appRouter}/>
-    </div>
-  )
+    <I18nextProvider i18n={i18next}> {/* Wrap with I18nextProvider */}
+      <div>
+        <RouterProvider router={appRouter} />
+      </div>
+    </I18nextProvider>
+  );
 }
 
 export default App;
