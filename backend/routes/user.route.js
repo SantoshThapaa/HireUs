67c41792ff50a59ba455fa2df8f3b45685f4caf6
SantoshThapaa <st708611@gmail.com>
2025-01-23 23:38:24 +0545

    User route modified

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,13 +1,18 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import {  login, logout,  register,  updateProfile } from "../controllers/user.controller.js";
+import {  forgotPassword, login, logout,  register,  resetPassword,  updateProfile, verifyEmail } from "../controllers/user.controller.js";
 import { singleUpload } from "../middlewares/multer.js";
+import { generateCV } from "../controllers/generateCV.js";
 
 const router = express.Router();
 
 router.route("/register").post(singleUpload, register);
+router.route("/verifyemail").post(verifyEmail);
+router.route("/forgot-password").post(forgotPassword);
+router.route("/reset-password/:id/:token").post(resetPassword);
 router.route("/login").post(login);
 router.route("/profile/update").post(isAuthenticated, singleUpload, updateProfile);
 router.route("/logout").get(logout);
+router.route("/generate-cv/:userId").get(generateCV);
 
 export default router;
